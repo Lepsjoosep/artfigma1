8e48ce5e7dada57714edddd3915eebfb71afbad7
lepsjoosep <lepsj12@gmail.com>
2026-05-13 11:26:16 +0300

    magazine lehe algus tehtud

---
+++ src/MagazineSection.jsx
@@ -0,0 +1,41 @@
+function MagazineSection() {
+    return (
+            <div className='p-10 pl-10 pr-10'>
+      <div className='flex flex-row justify-between'> 
+      </div >
+        <div className='flex flex-row justify-center pt-20'>
+          <div className='border-1 p-15'>
+            <img src="./src/Pictures/podcast-cover.png" alt="" className='h-110'/>
+            <h1 className='font-bold text-3xl w-100 h-19 mt-10'>The Problem of today's cultural development</h1>
+            <div className='flex flex-row gap-3 pt-20'>
+              <h1 className='font-bold'>Date</h1>
+              <h1>16.06.2022</h1>
+              <h1 className='font-bold'>Duration</h1>
+              <h1>1h 20 Min</h1>
+            </div>
+          </div>
+          <div className='border-1 p-15'>
+            <img src="./src/Pictures/podcast-cover3.png" alt="" className='h-110'/>
+            <h1 className='font-bold text-3xl w-100 h-19 mt-10'>The hidden messages of Jack Nielson</h1>
+            <div className='flex flex-row gap-3 pt-20'>
+              <h1 className='font-bold'>Date</h1>
+              <h1>16.06.2022</h1>
+              <h1 className='font-bold'>Duration</h1>
+              <h1>60 Min</h1>
+            </div>
+          </div>
+          <div className='border-1 p-15'>
+            <img src="./src/Pictures/podcast-cover2.png" alt="" className='h-110'/>
+            <h1 className='font-bold text-3x</div>l w-100 h-19 mt-10'>Behind the scenes of the street art culture</h1>
+            <div className='flex flex-row gap-3 pt-20'>
+              <h1 className='font-bold'>Date</h1>
+              <h1>16.06.2022</h1>
+              <h1 className='font-bold'>Duration</h1>
+              <h1>45 Min</h1>
+            </div>
+          </div>
+        </div>
+    </div>
+    );
+}
+export default MagazineSection;
--- src/magazine.jsx
+++ src/magazine.jsx
@@ -6,12 +6,13 @@ import Authors from './authors.jsx'
 import Podcast from './podcasts.jsx'
 import News from './news.jsx'
 import artImg from './Pictures/art.jpg'
+import MagazineSection from './MagazineSection.jsx'
 
 function Magazine() {
   return (
     <>
       <Header />
-      <h1 className='text-[350px] flex justify-center font-bold mt-[-100px] mb-[-200px]'>MAGAZINE</h1>
+      <h1 className='text-[275px] flex justify-center font-bold mt-[-100px] mb-[-200px] '>MAGAZINE</h1>
       <div className='mt-40 flex align-center justify-between p-10'>
         <h1 className='font-bold text-lg'>CATEGORIES</h1>
         <div>
@@ -21,7 +22,7 @@ function Magazine() {
           <button className="p-2 pt-1 pb-1 border rounded-3xl ml-5">SCULPTURES</button>
         </div>
       </div>
-      
+      <MagazineSection />
       <Footer />
     </>
   )

68175ae8bb3e0988e43a1c1d7d936ef701497201
Lepsjoosep <lepsj12@gmail.com>
2026-05-19 11:24:50 +0300

    Update MagazineSection.jsx

--- src/MagazineSection.jsx
+++ src/MagazineSection.jsx
@@ -1,37 +1,52 @@
 function MagazineSection() {
     return (
-            <div className='p-10 pl-10 pr-10'>
+            <div className=' pl-10 pr-10'>
       <div className='flex flex-row justify-between'> 
       </div >
         <div className='flex flex-row justify-center pt-20'>
-          <div className='border-1 p-15'>
-            <img src="./src/Pictures/podcast-cover.png" alt="" className='h-110'/>
-            <h1 className='font-bold text-3xl w-100 h-19 mt-10'>The Problem of today's cultural development</h1>
-            <div className='flex flex-row gap-3 pt-20'>
-              <h1 className='font-bold'>Date</h1>
-              <h1>16.06.2022</h1>
+          <div className='border-1 p-10'>
+            <div className="flex flex-row justify-between pb-10">
+              <h1 className="text-sm mt-2">16. March 2022</h1>
+              <button className="text-sm border-1 p-2 pl-3 pr-3 rounded-3xl">ART</button>
+            </div>
+            <img src="./src/Pictures/Hopedieslast.jpg" alt="" className='h-100 w-120 object-cover object-[70%_center]'/>
+            <h1 className='font-bold text-3xl w-100 h-15 mt-5'>Hope dies last</h1>
+            <h2 className="w-100 text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas dui id ornare arcu odio ut sem. Cras ornare arcu dui vivamus arcu felis bibendum ut. Porttitor leo a diam.</h2>
+            <div className='flex flex-row gap-3 pt-10'>
+              <h1 className='font-bold'>Text</h1>
+              <h1>Jakob Gronberg</h1>
               <h1 className='font-bold'>Duration</h1>
-              <h1>1h 20 Min</h1>
+              <h1 className="">1 Min</h1>
             </div>
           </div>
-          <div className='border-1 p-15'>
-            <img src="./src/Pictures/podcast-cover3.png" alt="" className='h-110'/>
-            <h1 className='font-bold text-3xl w-100 h-19 mt-10'>The hidden messages of Jack Nielson</h1>
-            <div className='flex flex-row gap-3 pt-20'>
-              <h1 className='font-bold'>Date</h1>
-              <h1>16.06.2022</h1>
+          <div className='border-1 p-10'>
+            <div className="flex flex-row justify-between pb-10">
+              <h1 className="text-sm mt-2">16. March 2022</h1>
+              <button className="text-sm border-1 p-2 pl-3 pr-3 rounded-3xl">ART</button>
+            </div>
+            <img src="./src/Pictures/art.jpg" alt="" className='h-100 w-120 object-cover object-[60%_center]'/>
+            <h1 className='font-bold text-3xl w-100 h-15 mt-5'>Don't close your eyes</h1>
+            <h2 className="w-100 text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas dui id ornare arcu odio ut sem. Cras ornare arcu dui vivamus arcu felis bibendum ut. Porttitor leo a diam.</h2>
+            <div className='flex flex-row gap-3 pt-10'>
+              <h1 className='font-bold'>Text</h1>
+              <h1>Jakob Gronberg</h1>
               <h1 className='font-bold'>Duration</h1>
-              <h1>60 Min</h1>
+              <h1 className="">1 Min</h1>
             </div>
           </div>
-          <div className='border-1 p-15'>
-            <img src="./src/Pictures/podcast-cover2.png" alt="" className='h-110'/>
-            <h1 className='font-bold text-3x</div>l w-100 h-19 mt-10'>Behind the scenes of the street art culture</h1>
-            <div className='flex flex-row gap-3 pt-20'>
-              <h1 className='font-bold'>Date</h1>
-              <h1>16.06.2022</h1>
+          <div className='border-1 p-10'>
+            <div className="flex flex-row justify-between pb-10">
+              <h1 className="text-sm mt-2">16. March 2022</h1>
+              <button className="text-sm border-1 p-2 pl-3 pr-3 rounded-3xl">SCULPTURES</button>
+            </div>
+            <img src="./src/Pictures/thebest.jpg" alt="" className='h-100 w-120 object-cover object-[70%_center]'/>
+            <h1 className='font-bold text-3xl w-100 h-15 mt-5'>The best art museums</h1>
+            <h2 className="w-100 text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas dui id ornare arcu odio ut sem. Cras ornare arcu dui vivamus arcu felis bibendum ut. Porttitor leo a diam.</h2>
+            <div className='flex flex-row gap-3 pt-10'>
+              <h1 className='font-bold'>Text</h1>
+              <h1>Jakob Gronberg</h1>
               <h1 className='font-bold'>Duration</h1>
-              <h1>45 Min</h1>
+              <h1 className="">1 Min</h1>
             </div>
           </div>
         </div>
